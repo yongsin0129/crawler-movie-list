1c8f9b8ef6d8b43230ca97a3c1b0f2b5b29c174c
yongsin0129 <yongsin0129@gmail.com>
2022-11-02 23:10:18 +0800

    refactor : using delay function instead

--- project-clickWebSite.ts
+++ project-clickWebSite.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Page } from 'puppeteer'
+import * as puppeteer from 'puppeteer'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -18,7 +18,7 @@ const webSiteURL = 'https://www.star-clicks.com/default'
     args: [`--window-size=1920,1080`]
     // devtools: true
   })
-  const page: Page = await browser.newPage()
+  const page: puppeteer.Page = await browser.newPage()
   let currentScreen = await page.evaluate(() => {
     return {
       width: window.screen.availWidth,
@@ -29,7 +29,7 @@ const webSiteURL = 'https://www.star-clicks.com/default'
   console.log(currentScreen)
   await page.setViewport(currentScreen)
 
-/********************************************************************************
+  /********************************************************************************
 *
           // 正式進入目標網頁
 *
@@ -43,18 +43,17 @@ const webSiteURL = 'https://www.star-clicks.com/default'
   //   ;(document.querySelector('a[href=login]') as HTMLElement).click()
   // }) 這兩種都可以達到 click 的目的
 
-  await page.waitForSelector('[id=Password]') // 等待登入頁面並輸入登入資訊
-  // await page.waitForNetworkIdle({ idleTime: 1000}) 
-  // 因為載入需要時間，本來想用 idle 等
-  // 但後來有些 idle 造成的bug , 改用 pwd 用 page.type 來打
+  // 等待登入頁面並輸入登入資訊
+  await delay(5000)
+  await page.waitForSelector('[id=Password]') 
+
   await page.evaluate(() => {
     ;(document.querySelector('[id=Email]')! as HTMLInputElement).value =
-      'yongsin0129@gmail.com'
+    'yongsin0129@gmail.com'
   })
-  await page.type('input[id=Password]', pwd)
+  await page.type('input[id=Password]', pwd) // 因為開 window 後，吃不到 .env 的 pwd , 所以改用 page.type 來打
 
-  await page.waitForNetworkIdle({ idleTime: 3000 }) // 等待 三秒 讓人手動 key 數字辨識
-
+  await delay(5000) // 等待五秒時間，讓人手動輸入數字辨識
   await page.click('input[id=Button1_input]') // 點擊登入
   console.log('成功點擊登入')
 
@@ -68,7 +67,7 @@ const webSiteURL = 'https://www.star-clicks.com/default'
   await page.click('.panel-blue')
   console.log('成功進入後台點擊頁面')
 
-  await page.waitForNetworkIdle({ idleTime: 10000 })
+  await delay(10000)
 
   await browser.close()
 })()
@@ -83,3 +82,10 @@ function delay (time: number) {
     setTimeout(resolve, time)
   })
 }
+
+// 以下兩種方式都可以做到等待
+// console.log(new Date())
+// await new Promise(r => setTimeout(r, 5000))
+// console.log(new Date())
+// await delay(5000)
+// console.log(new Date())
